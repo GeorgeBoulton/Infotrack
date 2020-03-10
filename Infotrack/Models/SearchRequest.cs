233444dfab2ff28f8d87a8143743b46a0424ef1e
George Boulton <george.boulton@condatis.com>
2020-03-10 10:24:11 +0000

    first commit

---
+++ Infotrack/Models/SearchRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Infotrack.Models
+{
+    public class SearchRequest
+    {
+        public string Keywords { get; set; }
+        public string SearchUrl { get; set; } = "https://google.com/search?num=100&q=";
+
+        public string FormatTerms()
+        {
+            return $"{this.SearchUrl}{Keywords.Replace(" ", "+")}";
+        }
+    }
+}
